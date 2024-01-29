dd22fbf6c12b4b6394fe1e20db48d8915775d63a
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2024-01-29 10:52:44 +0700

    Update contacts.sql

--- Contacts/Contacts-table/contacts.sql
+++ Contacts/Contacts-table/contacts.sql
@@ -3,3 +3,8 @@ CREATE TABLE contacts (
 `name` VARCHAR(30) NOT NULL,
 `PhoneNumber` VARCHAR(15) NOT NULL
 );
+
+INSERT INTO `contacts` (`id`, `name`, `PhoneNumber`) VALUES
+(1, 'Pham Kim Kien', 0987654321),
+(2, 'Dang Dinh Chieu', 0987654322),
+(3, 'Pham Tuan Anh', 0987654323);
