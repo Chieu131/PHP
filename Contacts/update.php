a0d0d4ec0b157b6858cf632d49624bee0ef074da
Chieu131 <142771810+Chieu131@users.noreply.github.com>
2024-01-29 10:52:53 +0700

    Update update.php

--- Contacts/update.php
+++ Contacts/update.php
@@ -36,7 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "ss", $param_name, $param_PhoneNumber, $param_id);
+        mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_PhoneNumber, $param_id);
         
         // Set parameters
         $param_name = $name;
